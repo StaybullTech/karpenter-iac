8c8abb80a0c19d42ae311174af20e6d6f361a54a
John Fotopoulos <john.fotopoulos@avlos.ai>
2024-05-27 19:48:37 +0300

    fix github deploy role.

--- iac/outputs.tf
+++ iac/outputs.tf
@@ -3,8 +3,8 @@ output "karpenter-controller-role-arn" {
   description = "The ARN of the Karpenter Controller role that was created."
 }
 
-output "karpenter-node-role-arn" {
-  value       = module.karpenter.node_iam_role_arn
+output "eks-deploy-role-arn" {
+  value       = module.iam_github_oidc_role.arn
   description = "The ARN of the Karpenter Node role that was created."
 }
 
