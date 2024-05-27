75566ab5dcf1ef2165014cd1ca970ed297c77d97
John Fotopoulos <john.fotopoulos@avlos.ai>
2024-05-27 20:55:37 +0300

    add yaml deployments

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ## Workflow and PAT(private-access-token)
 To be able to save secrets from within the worflow itself, we needed to create an extra token (PAT) and save it manually to our gh action secrets.
+
+> Warning: The EKS module sets the `cluster_endpoint_public_access` to 'true'. In a full fleshed environment we would create our own github runner and have it running in our VPC, therefore removing the need for the endpoint public access.
+
+> Warning:
--- iac/outputs.tf
+++ iac/outputs.tf
@@ -3,6 +3,11 @@ output "karpenter-controller-role-arn" {
   description = "The ARN of the Karpenter Controller role that was created."
 }
 
+output "karpenter-node-role-arn" {
+  value       = module.karpenter.node_iam_role_arn
+  description = "The Amazon Resource Name (ARN) specifying the node IAM role"
+}
+
 output "eks-deploy-role-arn" {
   value       = module.iam_github_oidc_role.arn
   description = "The ARN of the Karpenter Node role that was created."
